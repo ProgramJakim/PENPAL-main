267f53190f469af51bf5add1f701d40534139ca6
Niera <raquemannierose36@gmail.com>
2024-12-22 21:24:01 +0800

    main

--- FRONTEND/LogInPage.py
+++ FRONTEND/LogInPage.py
@@ -12,7 +12,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMessageBox, QMainWindow
 from SignUpPage import Ui_SignUp
-from HomePagetry import Ui_Homepage
+from FRONTEND.HomePage import Ui_Homepage
 import sys
 import os 
 import requests
